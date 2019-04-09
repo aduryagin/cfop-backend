44a6f1622d15f574c995134037f417a7891b09a4
Alexey Duryagin <aiduryagih@gmail.com>
2019-04-09 09:27:33 +0300

    get database url from environment

--- db/init.go
+++ db/init.go
@@ -1,15 +1,25 @@
 package db
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+// Instance - db instance
 var Instance *gorm.DB
 var err error
 
+// Init - db init
 func Init() {
-	Instance, err = gorm.Open("postgres", "host=database port=5432 user=postgres dbname=cfop sslmode=disable")
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	if databaseURL == "" {
+		databaseURL = "host=database port=5432 user=postgres dbname=cfop sslmode=disable"
+	}
+
+	Instance, err = gorm.Open("postgres", databaseURL)
 
 	if err != nil {
 		panic(err)
